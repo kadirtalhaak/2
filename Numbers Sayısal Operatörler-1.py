38f01b04d270642445e6ce98ee7d737fbffa339b
elmir <86436706+kadirtalhaak@users.noreply.github.com>
2023-11-02 23:10:15 +0300

    Add files via upload

---
+++ Numbers Sayısal Operatörler-1.py
@@ -0,0 +1,13 @@
+print(3+4)         # 7
+print(3.0+4)       # 7.0
+print(3.2+4)       # 7.2
+print(type(3.0+4)) # <class 'float'>
+print(3*3)         # 9
+print(3.0*3)       #9.0
+print(10/3)        # 3.3333333333333335
+print(10/5)        # 2.0
+print(type(10/5))  # <class 'float'>
+print(3+5*2)       # 13
+print((3+5)*2)     # 16
+print(3+5*6/2)     # 18.0
+print((3+5)*6/2)   # 24.0
